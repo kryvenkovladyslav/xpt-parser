38be8b1b34f285154c2fcc91e8d1e03365339d8b
DESKTOP-38Q3HR9\Vladyslav Kryvenko <onekryvenko.vladyslav@gmail.com>
2024-04-23 20:06:49 +0300

    Implemented required parser for parsing different parts of the document

---
+++ src/SasXptParser/SasXptLibraryHeaderRecordParser.cs
@@ -0,0 +1,59 @@
+﻿using SasXptParser.Abstract;
+using SasXptParser.Internal;
+using System.IO;
+
+namespace SasXptParser
+{
+    /// <summary>
+    /// Provides methods for parsing XPT Library Header Header Record into .NET object
+    /// </summary>
+    public class SasXptLibraryHeaderRecordParser : SasXptHeaderRecordParser<SasXptLibraryHeaderRecord>, ISasXptParser<SasXptLibraryHeaderRecord>
+    {
+        /// <summary>
+        /// Initializes the instance using ISasXptByteProcessor
+        /// </summary>
+        /// <param name="byteProcessor">Provided ISasXptByteProcessor</param>
+        public SasXptLibraryHeaderRecordParser(ISasXptByteProcessor byteProcessor) : base(byteProcessor, new SasXptLibraryHeaderRecordBytes())
+        {  }
+
+        /// <summary>
+        /// Parses the required XPT record of the document into specified .NET object
+        /// </summary>
+        /// <param name="sasXptDocumentStream"></param>
+        /// <returns>Parsed XPT header into .NET object</returns>
+        public virtual SasXptLibraryHeaderRecord ParseElement(Stream sasXptDocumentStream)
+        {
+            return this.ParseHeaderRecord(sasXptDocumentStream);
+        }
+
+        /// <summary>
+        /// Provides header record of the XPT document for parsing 
+        /// </summary>
+        /// <param name="sasXptDocumentStream">Provided XPT document stream</param>
+        /// <returns>Extracted current header of the XPT document</returns>
+        protected virtual string GetCurrentHeaderRecord(Stream sasXptDocumentStream)
+        {
+            var headerElementBytes = this.GetBytesFromStream(sasXptDocumentStream, SasXptLibraryHeaderRecordBytesDescriber.Header);
+            return this.ByteProcessor.ConvertBytesToString(headerElementBytes);
+        }
+
+        /// <summary>
+        /// Setups the required stream position for parsing the required part of the XPT document
+        /// </summary>
+        /// <param name="sasXptDocumentStream">Provided XPT document stream</param>
+        protected override void SetupRequiredStreamPosition(Stream sasXptDocumentStream)
+        {
+            sasXptDocumentStream.Position = SasXptPositionDescriber.LibraryHeaderRecordDefaultPosition;
+        }
+
+        /// <summary>
+        /// Throws the exception if the header does not satisfy the required header
+        /// </summary>
+        /// <param name="sasXptDocumentStream">Provided XPT document stream</param>
+        protected override void ThrowIfInvalidHeader(Stream sasXptDocumentStream)
+        {
+            var providedHeaderRecord = this.GetCurrentHeaderRecord(sasXptDocumentStream);
+            SasXptInvalidHeaderRecordException.ThrowIfInvalidHeaderRecord(providedHeaderRecord, SasXptHeaderRecordDescriber.SasXptLibraryHeader);
+        }
+    }
+}
---
+++ src/SasXptParser/SasXptParsingProcessor.cs
@@ -0,0 +1,55 @@
+﻿using SasXptParser.Abstract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SasXptParser
+{
+    /// <summary>
+    /// Provides methods for processing the XPT document
+    /// </summary>
+    public class SasXptParsingProcessor : ISasXptParsingProcessor
+    {
+        /// <summary>
+        /// Holds a collection of parsers for parsing the XPT document
+        /// </summary>
+        protected IEnumerable<ISasXptParser<ISasXptElement>> Parsers { get; private init; }
+
+        /// <summary>
+        /// Initializes the instance using a collection of parsers 
+        /// </summary>
+        /// <param name="parsers">Provided collection of parsers</param>
+        /// <exception cref="ArgumentNullException">ArgumentNullException is thrown if parsers are not provided</exception>
+        public SasXptParsingProcessor(IEnumerable<ISasXptParser<ISasXptElement>> parsers)
+        {
+            this.Parsers = parsers ?? throw new ArgumentNullException(nameof(parsers));
+        }
+
+        /// <summary>
+        /// Parses the entire XPT document
+        /// </summary>
+        /// <param name="sasXptDocumentStream">The stream representing XPT document</param>
+        /// <returns>Parsed XPT document</returns>
+        public virtual  SasXptDocument ParseDocument(Stream sasXptDocumentStream)
+        {
+            var parsedDocument = new SasXptDocument();
+
+            var properties = parsedDocument.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var requiredParser = this.Parsers.First(parser =>
+                {
+                    var parserType = parser.GetType();
+                    var returnType = parserType.GetMethod(nameof(parser.ParseElement)).ReturnType;
+                    return returnType == property.PropertyType;
+                });
+
+                property.SetValue(parsedDocument, requiredParser.ParseElement(sasXptDocumentStream));
+            }
+
+            return parsedDocument;
+        }
+    }
+}
